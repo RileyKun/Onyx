d3b56906e1a645e5607a18d2c7e09987bae6874a
PhoenixAceVFX <phoenixacevfx@trigon.systems>
2024-12-04 00:07:53 -0500

    Rewrite progress

--- Redline/Scripts/Editor/RedlineInfo.cs
+++ Redline/Scripts/Editor/RedlineInfo.cs
@@ -1,111 +1,73 @@
 using UnityEditor;
 using UnityEngine;
-using static UnityEngine.Color;
 
 namespace Redline.Scripts.Editor {
     [InitializeOnLoad]
-    public class RedlineInfo: EditorWindow {
-      private
-      const string Url = "https://github.com/Redline/Redline/";
-      private
-      const string Url1 = "https://trigon.systems/";
-      private
-      const string Link = "";
-      private
-      const string Link1 = "https://status.trigon.systems";
+    public class RedlineInfo : EditorWindow {
+        private const string Url = "https://github.com/Redline/Redline/";
+        private const string Url1 = "https://trigon.systems/";
+        private const string Link1 = "https://status.trigon.systems";
+        private static Vector2 _changeLogScroll;
+        private static GUIStyle _toolkitHeader;
+        private static GUIStyle _redlineBottomHeader;
+        private static GUIStyle _redlineHeaderLearnMoreButton;
+        private static GUIStyle _redlineBottomHeaderLearnMoreButton;
 
-      static RedlineInfo() {
-        EditorApplication.update -= DoSplashScreen;
-        EditorApplication.update += DoSplashScreen;
-      }
-
-      private static void DoSplashScreen() {
-        EditorApplication.update -= DoSplashScreen;
-        if (!EditorPrefs.HasKey("Redline_ShowInfoPanel")) {
-          EditorPrefs.SetBool("Redline_ShowInfoPanel", true);
+        static RedlineInfo() {
+            // Ensure that the splash screen displays correctly
+            EditorApplication.update -= DoSplashScreen;
+            EditorApplication.update += DoSplashScreen;
         }
 
-        if (EditorPrefs.GetBool("Redline_ShowInfoPanel"))
-        {
-        }
-      }
-
-      private static Vector2 _changeLogScroll;
-      private static GUIStyle _toolkitHeader;
-      private static GUIStyle _redlineBottomHeader;
-      private static GUIStyle _redlineHeaderLearnMoreButton;
-      private static GUIStyle _redlineBottomHeaderLearnMoreButton;
-
-      [MenuItem("Redline/Info", false, 500)]
-      private static void Init()
-      {
-        var window=(RedlineInfo)EditorWindow.GetWindow(typeof(RedlineInfo));
-        window.Show();
-      }
-      
-
-      public void OnEnable() {
-        titleContent = new GUIContent("Redline Info");
-
-        minSize = new Vector2(400, 720);;
-        _redlineBottomHeader = new GUIStyle();
-        _toolkitHeader = new GUIStyle {
-          normal = {
-              background = Resources.Load("RedlinePMHeader") as Texture2D,
-              textColor = white
-            },
-            fixedHeight = 200
-        };
-      }
-
-      public void OnGUI() {
-        GUILayout.Box("", _toolkitHeader);
-        _redlineHeaderLearnMoreButton = EditorStyles.miniButton;
-        _redlineHeaderLearnMoreButton.normal.textColor = black;
-        _redlineHeaderLearnMoreButton.fontSize = 12;
-        _redlineHeaderLearnMoreButton.border = new RectOffset(10, 10, 10, 10);
-        var texture = AssetDatabase.GetBuiltinExtraResource < Texture2D > ("UI/Skin/UISprite.psd");
-        _redlineHeaderLearnMoreButton.normal.background = texture;
-        _redlineHeaderLearnMoreButton.active.background = texture;
-        GUILayout.Space(4);
-        GUI.backgroundColor = new Color(
-          EditorPrefs.GetFloat("RedlineColor_R"),
-          EditorPrefs.GetFloat("RedlineColor_G"),
-          EditorPrefs.GetFloat("RedlineColor_B"),
-          EditorPrefs.GetFloat("RedlineColor_A")
-        );
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("The Black Arms Ultimate Development Kit")) {
-          Application.OpenURL(Url);
+        private static void DoSplashScreen() {
+            EditorApplication.update -= DoSplashScreen;
+            // Ensure "Redline_ShowInfoPanel" exists in EditorPrefs
+            if (!EditorPrefs.HasKey("Redline_ShowInfoPanel")) {
+                EditorPrefs.SetBool("Redline_ShowInfoPanel", true);
+            }
         }
 
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Trigon.Systems")) {
-          Application.OpenURL(Url1 + Link);
+        [MenuItem("Redline/Info", false, 500)]
+        private static void Init() {
+            var window = (RedlineInfo)EditorWindow.GetWindow(typeof(RedlineInfo));
+            window.Show();
         }
 
-        GUILayout.EndHorizontal();
-        GUILayout.Space(4);
-        //Update assets
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Status")) {
-          Application.OpenURL(Link1);
+        public void OnEnable() {
+            titleContent = new GUIContent("Redline Info");
+            minSize = new Vector2(400, 720);
+
+            _redlineBottomHeader = new GUIStyle();
+            _toolkitHeader = new GUIStyle {
+                normal = {
+                    background = Resources.Load("RedlinePMHeader") as Texture2D,
+                    textColor = Color.white
+                },
+                fixedHeight = 200
+            };
         }
 
-        GUILayout.EndHorizontal();
-        GUILayout.Space(4);
-        GUILayout.Label("Redline Version 1.0.1");
-        GUILayout.Space(2);
-        GUILayout.Label("Redline imported correctly if you are seeing this");
-        _changeLogScroll = GUILayout.BeginScrollView(_changeLogScroll, GUILayout.Width(390));
+        public void OnGUI() {
+            GUILayout.Box("", _toolkitHeader);
+            SetupButtonStyle();
 
-        GUILayout.Label(
-          @" 
+            // Displaying URLs
+            DisplayLinkButton("The Black Arms Ultimate Development Kit", Url);
+            DisplayLinkButton("Trigon.Systems", Url1);
+            DisplayLinkButton("Status", Link1);
+
+            GUILayout.Space(4);
+            GUILayout.Label("Redline Version 1.0.1");
+            GUILayout.Label("Redline imported correctly if you are seeing this");
+
+            // Changelog ScrollView
+            _changeLogScroll = GUILayout.BeginScrollView(_changeLogScroll, GUILayout.Width(390));
+            GUILayout.Label(
+                @"
 == Redline Package Manager ==
 
 This Unity Kit is hopefully providing everything you need
-I shot the update prompt for annoying up to date user
+I shot the update prompt for annoying up-to-date users
 DOCKABLE WINDOWS ARE HERE!
 
 ---------------------------------------------------------
@@ -122,15 +84,38 @@ https://rpm.c0dera.in/discord
 ---------------------------------------------------------
 ∞∞∞∞Contributors to Redline Unity Kit∞∞∞∞
 > Developer: PhoenixAceVFX
-- Contributor : WTFBlaze -Made the import system
+- Contributor: WTFBlaze - Made the import system
 ============================================
-          ");
-          GUILayout.EndScrollView(); GUILayout.Space(4);
+                ");
+            GUILayout.EndScrollView();
 
-          GUILayout.Box("", _redlineBottomHeader); _redlineBottomHeaderLearnMoreButton = EditorStyles.miniButton; _redlineBottomHeaderLearnMoreButton.normal.textColor = black; _redlineBottomHeaderLearnMoreButton.fontSize = 10; _redlineBottomHeaderLearnMoreButton.border = new RectOffset(10, 10, 10, 10); _redlineBottomHeaderLearnMoreButton.normal.background = texture; _redlineBottomHeaderLearnMoreButton.active.background = texture;
+            GUILayout.Space(4);
+            GUILayout.Box("", _redlineBottomHeader);
 
-          GUILayout.FlexibleSpace(); GUILayout.BeginHorizontal(); EditorPrefs.SetBool("Redline_ShowInfoPanel",
-            GUILayout.Toggle(EditorPrefs.GetBool("Redline_ShowInfoPanel"), "Show at startup")); GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal();
+            EditorPrefs.SetBool("Redline_ShowInfoPanel", GUILayout.Toggle(EditorPrefs.GetBool("Redline_ShowInfoPanel"), "Show at startup"));
+            GUILayout.EndHorizontal();
         }
-      }
+
+        // Helper method to set up button styles
+        private void SetupButtonStyle() {
+            _redlineHeaderLearnMoreButton = EditorStyles.miniButton;
+            _redlineHeaderLearnMoreButton.normal.textColor = Color.black;
+            _redlineHeaderLearnMoreButton.fontSize = 12;
+            _redlineHeaderLearnMoreButton.border = new RectOffset(10, 10, 10, 10);
+            var texture = AssetDatabase.GetBuiltinExtraResource<Texture2D>("UI/Skin/UISprite.psd");
+            _redlineHeaderLearnMoreButton.normal.background = texture;
+            _redlineHeaderLearnMoreButton.active.background = texture;
+        }
+
+        // Helper method to display buttons that open URLs
+        private void DisplayLinkButton(string label, string url) {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(label)) {
+                Application.OpenURL(url);
+            }
+            GUILayout.EndHorizontal();
+        }
     }
+}
