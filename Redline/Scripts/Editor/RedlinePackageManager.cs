58fa29770250973f3dd29d4673a432955a33a5c6
PhoenixAceVFX <phoenixacevfx@trigon.systems>
2024-12-04 00:03:58 -0500

    Rewrite progress
    I'm having to push these one at a time, thanks github

--- Redline/Scripts/Editor/RedlinePackageManager.cs
+++ Redline/Scripts/Editor/RedlinePackageManager.cs
@@ -1,115 +1,132 @@
-﻿using System;
-using System.ComponentModel;
+using System;
 using System.IO;
-using System.Net;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
 
 namespace Redline.Scripts.Editor {
-  public abstract class RedlineImportManager {
-    private
-    const string V = "https://c0dera.in/Redline/api/assets/";
+    public abstract class RedlineImportManager {
+        private const string V = "https://c0dera.in/Redline/api/assets/";
+        public const string ConfigName = "importConfig.json";
+        public static string ServerUrl = V;
+        private const string InternalServerUrl = V;
 
-    public const string ConfigName = "importConfig.json";
-    public static string ServerUrl = V;
-    private const string InternalServerUrl = V;
-
-    public static void DownloadAndImportAssetFromServer(string assetName) {
-      if (File.Exists(RedlineSettings.GetAssetPath() + assetName)) {
-        RedlineLog(assetName + " exists. Importing it..");
-        ImportDownloadedAsset(assetName);
-      } else {
-        RedlineLog(assetName + " does not exist. Starting download..");
-        DownloadFile(assetName);
-      }
-    }
-
-    private static void DownloadFile(string assetName) {
-      var w = new WebClient();
-      w.Headers.Set(HttpRequestHeader.UserAgent, "Webkit Gecko wHTTPS (Keep Alive 55)");
-      w.QueryString.Add("assetName", assetName);
-      w.DownloadFileCompleted += FileDownloadCompleted;
-      w.DownloadProgressChanged += FileDownloadProgress;
-      var url = ServerUrl + assetName;
-      w.DownloadFileAsync(new Uri(url), RedlineSettings.GetAssetPath() + assetName);
-    }
-
-    public static void DeleteAsset(string assetName) {
-      File.Delete(RedlineSettings.GetAssetPath() + assetName);
-    }
-
-    public static void UpdateConfig() {
-      var w = new WebClient();
-      w.Headers.Set(HttpRequestHeader.UserAgent, "Webkit Gecko wHTTPS (Keep Alive 55)");
-      w.DownloadFileCompleted += ConfigDownloadCompleted;
-      w.DownloadProgressChanged += FileDownloadProgress;
-      var url = InternalServerUrl + ConfigName;
-      w.DownloadFileAsync(new Uri(url), RedlineSettings.ProjectConfigPath + "update_" + ConfigName);
-    }
-
-    private static void ConfigDownloadCompleted(object sender, AsyncCompletedEventArgs e) {
-      if (e.Error == null) {
-        File.Delete(RedlineSettings.ProjectConfigPath + ConfigName);
-        File.Move(RedlineSettings.ProjectConfigPath + "update_" + ConfigName,
-          RedlineSettings.ProjectConfigPath + ConfigName);
-        RedlinePackageManager.LoadJson();
-
-        EditorPrefs.SetInt("Redline_configImportLastUpdated",
-          (int) DateTimeOffset.UtcNow.ToUnixTimeSeconds());
-        RedlineLog("Import Config has been updated!");
-      } else {
-        RedlineLog("Import Config could not be updated!");
-      }
-    }
-
-    private static void FileDownloadCompleted(object sender, AsyncCompletedEventArgs e) {
-      var assetName = ((WebClient) sender).QueryString["assetName"];
-      if (e.Error == null) {
-        RedlineLog("Download of file " + assetName + " completed!");
-      } else {
-        DeleteAsset(assetName);
-        RedlineLog("Download of file " + assetName + " failed!");
-      }
-    }
-
-    private static void FileDownloadProgress(object sender, DownloadProgressChangedEventArgs e) {
-      var progress = e.ProgressPercentage;
-      var assetName = ((WebClient) sender).QueryString["assetName"];
-      switch (progress) {
-      case < 0:
-        return;
-      case >= 100:
-        EditorUtility.ClearProgressBar();
-        break;
-      default:
-        EditorUtility.DisplayProgressBar("Download of " + assetName,
-          "Downloading " + assetName + ". Currently at: " + progress + "%",
-          (progress / 100F));
-        break;
-      }
-    }
-
-    public static void CheckForConfigUpdate() {
-      if (EditorPrefs.HasKey("Redline_configImportLastUpdated")) {
-        var lastUpdated = EditorPrefs.GetInt("Redline_configImportLastUpdated");
-        var currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-        if (currentTime - lastUpdated < 3600) {
-          Debug.Log("Not updating config: " + (currentTime - lastUpdated));
-          return;
+        // Starts the download and import process
+        public static async void DownloadAndImportAssetFromServer(string assetName) {
+            string assetPath = RedlineSettings.GetAssetPath() + assetName;
+            if (File.Exists(assetPath)) {
+                RedlineLog($"{assetName} exists. Importing it..");
+                ImportDownloadedAsset(assetPath);
+            } else {
+                RedlineLog($"{assetName} does not exist. Starting download..");
+                await DownloadFile(assetName);
+            }
         }
-      }
 
-      RedlineLog("Updating import config");
-      UpdateConfig();
-    }
+        // Download the asset file asynchronously
+        private static async Task DownloadFile(string assetName) {
+            string url = ServerUrl + assetName;
+            string downloadPath = RedlineSettings.GetAssetPath() + assetName;
 
-    private static void RedlineLog(string message) {
-      Debug.Log("[Redline] AssetDownloadManager: " + message);
-    }
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(url)) {
+                webRequest.SetRequestHeader("User-Agent", "Webkit Gecko wHTTPS (Keep Alive 55)");
 
-    private static void ImportDownloadedAsset(string assetName) {
-      AssetDatabase.ImportPackage(RedlineSettings.GetAssetPath() + assetName, true);
+                // Start download
+                var operation = webRequest.SendWebRequest();
+
+                // While downloading, show the progress
+                while (!operation.isDone) {
+                    EditorUtility.DisplayProgressBar("Downloading " + assetName,
+                        "Downloading " + assetName + ". Currently at: " + (int)(operation.progress * 100) + "%",
+                        operation.progress);
+                    await Task.Yield();  // Yield control back to the Unity editor thread
+                }
+
+                if (webRequest.result == UnityWebRequest.Result.Success) {
+                    File.WriteAllBytes(downloadPath, webRequest.downloadHandler.data);
+                    RedlineLog($"Download of {assetName} completed!");
+                    ImportDownloadedAsset(downloadPath);
+                } else {
+                    RedlineLog($"Download of {assetName} failed! Error: {webRequest.error}");
+                }
+
+                EditorUtility.ClearProgressBar();
+            }
+        }
+
+        // Delete the asset from the system
+        public static void DeleteAsset(string assetName) {
+            string assetPath = RedlineSettings.GetAssetPath() + assetName;
+            if (File.Exists(assetPath)) {
+                File.Delete(assetPath);
+                RedlineLog($"{assetName} deleted.");
+            }
+        }
+
+        // Update the configuration file
+        public static async void UpdateConfig() {
+            string url = InternalServerUrl + ConfigName;
+            string updatePath = RedlineSettings.ProjectConfigPath + "update_" + ConfigName;
+
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(url)) {
+                webRequest.SetRequestHeader("User-Agent", "Webkit Gecko wHTTPS (Keep Alive 55)");
+
+                // Start download
+                var operation = webRequest.SendWebRequest();
+
+                // While downloading, show the progress
+                while (!operation.isDone) {
+                    EditorUtility.DisplayProgressBar("Downloading Config", "Downloading import config...", operation.progress);
+                    await Task.Yield();
+                }
+
+                if (webRequest.result == UnityWebRequest.Result.Success) {
+                    File.WriteAllBytes(updatePath, webRequest.downloadHandler.data);
+                    RedlineLog("Config download completed!");
+
+                    // Replace old config file
+                    string oldConfigPath = RedlineSettings.ProjectConfigPath + ConfigName;
+                    if (File.Exists(oldConfigPath)) {
+                        File.Delete(oldConfigPath);
+                    }
+
+                    File.Move(updatePath, RedlineSettings.ProjectConfigPath + ConfigName);
+                    RedlinePackageManager.LoadJson();
+                    EditorPrefs.SetInt("Redline_configImportLastUpdated", (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                } else {
+                    RedlineLog("Config download failed! Error: " + webRequest.error);
+                }
+
+                EditorUtility.ClearProgressBar();
+            }
+        }
+
+        // Check for updates to the configuration file
+        public static void CheckForConfigUpdate() {
+            if (EditorPrefs.HasKey("Redline_configImportLastUpdated")) {
+                var lastUpdated = EditorPrefs.GetInt("Redline_configImportLastUpdated");
+                var currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+                // Skip update if the config was recently updated within the last hour
+                if (currentTime - lastUpdated < 3600) {
+                    Debug.Log("Not updating config: " + (currentTime - lastUpdated) + " seconds since last update.");
+                    return;
+                }
+            }
+
+            RedlineLog("Updating import config");
+            UpdateConfig();
+        }
+
+        // Log function for Redline operations
+        private static void RedlineLog(string message) {
+            Debug.Log("[Redline] AssetDownloadManager: " + message);
+        }
+
+        // Import the downloaded asset package
+        private static void ImportDownloadedAsset(string assetPath) {
+            AssetDatabase.ImportPackage(assetPath, true);
+        }
     }
-  }
 }
